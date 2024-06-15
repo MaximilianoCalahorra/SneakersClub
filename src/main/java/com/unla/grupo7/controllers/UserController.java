ddc31b69063eb4bbf6bc6f6477a7d0c2b31962e8
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-06-15 15:12:11 -0300

    Comentamos userController

--- src/main/java/com/unla/grupo7/controllers/UserController.java
+++ src/main/java/com/unla/grupo7/controllers/UserController.java
@@ -1,18 +1,18 @@
 package com.unla.grupo7.controllers;
-
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.unla.grupo7.entities.User;
 import com.unla.grupo7.helpers.ViewRouteHelper;
 
 
 @Controller
 public class UserController {
-
+	
+	//1- CUANTO SE PETICIONA /login ENVIAMOS LA VISTA DE LOGUEO ---> user/login.
+	//AÑADIMOS AUTENTICACION CON springSecurity
 	@GetMapping("/login")
 	public String login(Model model,
 						@RequestParam(name="error",required=false) String error,
@@ -21,12 +21,14 @@ public class UserController {
 		model.addAttribute("logout", logout);
 		return ViewRouteHelper.USER_LOGIN;
 	}
-
+	
+	//2- CUANDO SE PETICIONA /logout ENVIAMOS LA VISTA DE LOGOUT ---> user/logout. 
 	@GetMapping("/logout")
 	public String logout(Model model) {
 		return ViewRouteHelper.USER_LOGOUT;
 	}
-
+	
+	
 	@GetMapping("/loginsuccess")
 	public String loginCheck() {
 		//User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();

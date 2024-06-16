494d41021724b37e4bbe6c98902b1bbfd4aa0ed7
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2024-06-16 19:26:17 -0300

    Modificando atributo de la entity Stock

--- src/main/java/com/unla/grupo7/entities/Stock.java
+++ src/main/java/com/unla/grupo7/entities/Stock.java
@@ -1,17 +1,11 @@
 package com.unla.grupo7.entities;
 
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -26,9 +20,6 @@ public class Stock {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int stockId;
-	
-	@OneToMany(fetch=FetchType.LAZY, mappedBy="stock")
-	private Set<Lot> lots = new HashSet<>();
 
 	//one to one  con producto desde stock
 	@OneToOne(cascade = CascadeType.ALL)

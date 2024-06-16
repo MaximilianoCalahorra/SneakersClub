e08f2445b61b07e3ea05f579e6df26477e5b7f3d
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-06-16 10:40:02 -0300

    Realizamos modificacion de IUserRepository

--- src/main/java/com/unla/grupo7/repositories/IUserRepository.java
+++ src/main/java/com/unla/grupo7/repositories/IUserRepository.java
@@ -13,6 +13,6 @@ import com.unla.grupo7.entities.User;
 @Repository("userRepository")
 public interface IUserRepository extends JpaRepository<User, Serializable> {
 
-	@Query("SELECT u FROM User u JOIN FETCH u.userRoles WHERE u.username = (:username)")
-	public abstract User findByUsernameAndFetchUserRolesEagerly(@Param("username") String username);
+    @Query("SELECT u FROM User u JOIN FETCH u.userRoles WHERE u.username = (:username)")
+    public abstract User findByUsernameAndFetchUserRolesEagerly(@Param("username") String username);
 }

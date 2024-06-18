b22d47a400c76ec02f8e010fd84947c3fb2f32b0
Federico Noceti <nfedericotomas@gmail.com>
2024-06-17 22:52:32 -0300

    Agregando funcionalidad a purchaseController

--- src/main/java/com/unla/grupo7/controllers/PurchaseController.java
+++ src/main/java/com/unla/grupo7/controllers/PurchaseController.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.unla.grupo7.entities.Product;
 import com.unla.grupo7.entities.Purchase;
-import com.unla.grupo7.entities.User;
 import com.unla.grupo7.helpers.ViewRouteHelper;
 import com.unla.grupo7.services.IProductService;
 import com.unla.grupo7.services.IPurchaseService;
 import com.unla.grupo7.services.IStockService;
-import com.unla.grupo7.services.implementation.UserService;
 
 @Controller
 @RequestMapping("/purchases")
@@ -25,48 +23,53 @@ public class PurchaseController
 	private IPurchaseService purchaseService;
 	private IStockService stockService;
 	private IProductService productService;
-	private UserService userService;
+
 	
 	//Constructor:
-	public PurchaseController(IPurchaseService purchaseService, IStockService stockService, IProductService productService, UserService userService) 
+	public PurchaseController(IPurchaseService purchaseService, IStockService stockService, IProductService productService) 
 	{
 		this.purchaseService = purchaseService;
 		this.stockService = stockService;
 		this.productService = productService;
-		this.userService = userService;
+	
 	}
 	
 	//Presentamos el producto con toda su información y un formulario donde se permite la compra del mismo:
-	@GetMapping("/purchaseForm/{productId}{username}")
-	public ModelAndView purchaseForm(@PathVariable int productId, @PathVariable String username) throws Exception 
+	@GetMapping("/purchaseForm/{productId}")
+	public ModelAndView purchaseForm(@PathVariable int productId) throws Exception 
 	{
 		ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.PURCHASE_FORM); //Indicamos la vista a cargar.
 		Product product = productService.findByProductId(productId); //Obtenemos el producto por su id.
 		modelAndView.addObject("product", product); //Agregamos el producto a la vista.
-		modelAndView.addObject("username", username); //Agregamos el username a la vista.
 		return modelAndView; //Retornamos la vista.
 	}
 	
 	//Agregamos los datos de la compra a la base de datos en caso de que alcance el stock:
 	@PostMapping("/purchaseSave")
-	public String create(@RequestParam("productId")int productId, @RequestParam("username")String username, @RequestParam("amount")int amount,
+	public ModelAndView create(@RequestParam("productId")int productId, @RequestParam("amount")int amount,
 						 @RequestParam("methodOfPay")String methodOfPay) 
+	
 	{
-		String message = "";
+		ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.POSTPURCHASE);
+		
 		try 
 		{
+			
 			stockService.availableStock(productId, amount); //Verificamos que el stock sea suficiente. En caso de que no levanta la excepción.
 			Product product = productService.findByProductId(productId); //Obtenemos el producto a comprar.
+			System.out.println(product.getBrand());
 			double purchasePrice = product.getSalePrice() * amount; //Calculamos el total de la compra.
-			User user = userService.findByUsername(username); //Obtenemos el usuario que realizó la compra.
-			Purchase purchase = new Purchase(user, product, purchasePrice, amount, methodOfPay); //Generamos un objeto 'Purchase' con los datos de la compra.
+			Purchase purchase = new Purchase(product, purchasePrice, amount, methodOfPay); //Generamos un objeto 'Purchase' con los datos de la compra.
 			purchaseService.insert(purchase); //Guardamos la compra en la base de datos.
-			message = "Payment made successfully. Thanks for your purchase!";
+			
+			modelAndView.addObject("product", product);
+			
+			modelAndView.addObject("purchase", purchase);
 		} 
 		catch(Exception e) 
 		{
-			message = e.getMessage();
+			e.getMessage();
 		}
-		return message;
+		return modelAndView;
 	}
 }

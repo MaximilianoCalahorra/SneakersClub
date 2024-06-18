c424d7fac4de599d43c2a2804e85191722dff17e
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2024-06-18 10:53:49 -0300

    Modificando repositories

--- src/main/java/com/unla/grupo7/repositories/IPurchaseRepository.java
+++ src/main/java/com/unla/grupo7/repositories/IPurchaseRepository.java
@@ -2,7 +2,6 @@ package com.unla.grupo7.repositories;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Optional;
 import java.time.LocalDateTime;
 
 import org.springframework.data.jpa.repository.JpaRepository;

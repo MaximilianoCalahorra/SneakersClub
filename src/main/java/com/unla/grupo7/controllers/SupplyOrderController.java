5fca212f1566fb9e61d3921ab0fa348a8fdd424e
Federico Noceti <nfedericotomas@gmail.com>
2024-06-18 14:36:53 -0300

    Agregando funcionalidad a supplyOrderController

--- src/main/java/com/unla/grupo7/controllers/SupplyOrderController.java
+++ src/main/java/com/unla/grupo7/controllers/SupplyOrderController.java
@@ -1,5 +1,6 @@
 package com.unla.grupo7.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -10,23 +11,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
 import com.unla.grupo7.entities.Product;
 import com.unla.grupo7.entities.SupplyOrder;
 import com.unla.grupo7.helpers.ViewRouteHelper;
+import com.unla.grupo7.services.ILotService;
 import com.unla.grupo7.services.IProductService;
 import com.unla.grupo7.services.ISupplyOrderService;
 
 @Controller
-@RequestMapping("/supplyOrder")
+@RequestMapping("/supplyOrders")
 public class SupplyOrderController {
 	
 	private ISupplyOrderService supplyOrderService;
 	private IProductService productService;
+	private ILotService lotService;
 
-	public SupplyOrderController(ISupplyOrderService supplyOrderService, IProductService productService) {
+	public SupplyOrderController(ISupplyOrderService supplyOrderService, IProductService productService, ILotService lotService) {
 		super();
 		this.supplyOrderService = supplyOrderService;
 		this.productService = productService;
+		this.lotService = lotService;
 	}
 	
 	//1- AGREGAR PRODUCTOS
@@ -60,8 +65,6 @@ public class SupplyOrderController {
 	{
 		ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.SUPPLY_ORDER_SAVE);
 		
-		System.out.println("Product ID" + supplyOrder.getProduct().getName());
-		
 		try {	
 			
 			supplyOrderService.insert(supplyOrder);
@@ -80,26 +83,41 @@ public class SupplyOrderController {
 	@GetMapping("/supplyOrders")
 	public ModelAndView supplyOrders() {
 		ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.SUPPLY_ORDERS);
-		List <SupplyOrder> listaSuppyOrdersInProcess = supplyOrderService.findByState("In process");
-		List <SupplyOrder> listaSuppyOrdersDelivered = supplyOrderService.findByState("Delivered");
-		modelAndView.addObject("listaSupplyOrdersInProcess", listaSuppyOrdersInProcess);
-		modelAndView.addObject("listaSupplyOrdersDelivered", listaSuppyOrdersDelivered);
+		List <SupplyOrder> listaSupplyOrdersInProcess = supplyOrderService.findByState("In process");
+		List <SupplyOrder> listaSupplyOrdersDelivered = supplyOrderService.findByState("Delivered");
+		List<SupplyOrder> listaAux = new ArrayList<SupplyOrder>();
+		
+		for (SupplyOrder supplyOrder : listaSupplyOrdersDelivered) {
+			
+			if (lotService.findBySupplyOrder(supplyOrder.getSupplyOrderId()) == null) {
+				
+			
+				listaAux.add(supplyOrder);
+				
+			}
+			
+		}
+		modelAndView.addObject("listaSupplyOrdersInProcess", listaSupplyOrdersInProcess);
+		modelAndView.addObject("listaSupplyOrdersDelivered", listaAux);
 		return modelAndView;
 	}
 	
 	@GetMapping("/registerDelivered/{supplyOrderId}")
 	public RedirectView registerDelivered(@PathVariable int supplyOrderId) 
 	{
+		
 		try
 		{
 			SupplyOrder supplyOrder = supplyOrderService.findBySupplyOrderId(supplyOrderId); //Obtenemos el SupplyOrder a modificar.
 			supplyOrder.setState("Delivered"); //Seteamos el estado de la SupplyOrder a entregado como solicitó el administrador.
 			supplyOrderService.insert(supplyOrder); //Modificamos la SupplyOrder en la base de datos.
+	
 		} 
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		} 
-		return new RedirectView (ViewRouteHelper.REDIRECT_SUPPLY_ORDERS); //Redireccionamos a la lista de SupplyOrder.
+		
+		return new RedirectView(ViewRouteHelper.REDIRECT_SUPPLY_ORDERS); //Redireccionamos a la lista de SupplyOrder.
 	}
 }

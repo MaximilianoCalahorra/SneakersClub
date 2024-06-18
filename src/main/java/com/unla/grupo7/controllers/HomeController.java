5b3c34afd634a053f39433bf2278b2d1df19d9fc
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-06-17 23:11:22 -0300

    Realizamos modificacion al HomeController. Agregamos objectWrapper para poder pasar mas datos a las vistas. Agregamos mas funcionalidades

--- src/main/java/com/unla/grupo7/controllers/HomeController.java
+++ src/main/java/com/unla/grupo7/controllers/HomeController.java
@@ -1,4 +1,5 @@
 package com.unla.grupo7.controllers;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.unla.grupo7.entities.Product;
+import com.unla.grupo7.entities.Stock;
 import com.unla.grupo7.helpers.ViewRouteHelper;
 import com.unla.grupo7.services.IProductService;
+import com.unla.grupo7.services.IStockService;
 
 @Controller
 @RequestMapping("/")
@@ -19,11 +22,37 @@ public class HomeController {
 	//DEFINIMOS INTERFACE DEL SERVICIO DE PRODUCTO.
 	//ESTO ES PARA PODER MOSTRAR LOS PRODUCTOS GUARDADOS EN LA BD EN EL INDEX DEL ADMIN.
 	private IProductService productService;
+	private IStockService stockService;
 		
 	//CONSTRUCTOR DEL SERVICIO
-	public HomeController(IProductService productService) {
+	public HomeController(IProductService productService, IStockService stockService) {
 		super();
 		this.productService = productService;
+		this.stockService = stockService;
+	}
+	
+	//CONSTRUIMOS UNA CLASE WRAPPER PARA PODER PASAR EL STOCK Y LOS PRODUCTOS A LA VISTA DEL ADMIN Y
+	//PODER RECORRERLOS DENTRO DE LA MISMA.
+	public class ProductStockWrapper {
+		
+		//ATRIBUTOS
+		private Product product;
+		private Stock stock;
+		
+		//CONSTRUCTOR
+		public ProductStockWrapper (Product product, Stock stock) {
+			this.product = product;
+			this.stock = stock;
+		}
+				
+		//GETTERS
+		public Product getProduct() {
+			return product;
+		}
+		public Stock getStock () {
+			
+			return stock;
+		}		
 	}
 
 	//1- CUANDO SE PETICIONA /index ENVIAMOS LA VISTA PRINCIPAL --> home/index.
@@ -39,9 +68,35 @@ public class HomeController {
 		
 			modelAndView = new ModelAndView(ViewRouteHelper.ADMIN_INDEX);
 			
-			List <Product> listaProductos = productService.findByEnabled(true);
-			modelAndView.addObject("listaProducts", listaProductos);
-		
+			//CREAMOS UNA LISTA DEL TIPO ProductStockWrapper
+			List <ProductStockWrapper> listaProductosStock = new ArrayList <ProductStockWrapper>();
+			
+			//OBTENEMOS LA LISTA DE PRODUCTOS
+			List <Product> listaProductos = productService.getAll();
+			
+			for (int i = 0; i < listaProductos.size(); i ++) {
+				
+				Product product = listaProductos.get(i);
+				
+				Stock stock = null;
+				
+				try {
+					stock = stockService.findByProduct(product.getProductId());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				//CREAMOS UN productStockWrapper Y LO CARGAMOS CON EL STOCK Y SU PRODUCTO CORRESPONDIENTE.
+				ProductStockWrapper productStock = new ProductStockWrapper (product, stock);
+				
+				//LO AÑADIMOS A LA LISTA
+				listaProductosStock.add(productStock);
+			}
+			
+			//PASAMOS EL ProductStockWrapper A LA VISTA PARA PODER RECORRER SU LISTA DENTRO DE LA VISTA.
+			modelAndView.addObject("listaProductosStock", listaProductosStock);
+			
 		//SI ES UN USER...	
 		} else {
 			
@@ -53,18 +108,4 @@ public class HomeController {
 		
 		return modelAndView;
 	}
-	
-	
-	/*COSITAS EXTRAS
-	@GetMapping("/hello/{name}")
-	public ModelAndView helloParams2(@PathVariable("name") String name) {
-		ModelAndView mV = new ModelAndView(ViewRouteHelper.HELLO);
-		mV.addObject("name", name);
-		return mV;
-	}
-
-	@GetMapping("/")
-	public RedirectView redirectToHomeIndex() {
-		return new RedirectView(ViewRouteHelper.ROUTE);
-	}*/
 }
